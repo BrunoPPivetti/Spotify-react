9dea1b9f55d3106b4a6114c3845e48d04efa2d9d
brunoppivetti <brunopivetti2004@gmail.com>
2025-02-05 19:19:43 -0300

    Explorer section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './header/header';
 import Sidebar from './sidebar/sidebar'
+import Main from './main/main'
 
 function App() {
   return (
       <>
         <Header/>
         <Sidebar/>
+        <Main/>
       </>
   );
 }
